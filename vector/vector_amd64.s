2a8ab2d95cf2176a919e84e2a02d23404416bf5b
Derek Parker <parkerderek86@gmail.com>
2013-10-06 18:12:45 -0500

    Align columns

--- vector/vector_amd64.s
+++ vector/vector_amd64.s
@@ -6,21 +6,21 @@ TEXT ·Find+0(SB),$0
   MOVLQSX CX, CX        // CX as int64
 
 start:
-  CMPQ SI, CX
-  JG notfound
-  CMPQ (BX), DX
-  JNE notequal
-  JE found
+  CMPQ    SI, CX
+  JG      notfound
+  CMPQ    (BX), DX
+  JNE     notequal
+  JE      found
 
 found:
-  MOVQ $1, return+32(FP)
-  RET ,
+  MOVQ    $1, return+32(FP)
+  RET
 
 notequal:
-  INCQ SI
-  ADDQ $8, BX
-  JMP start
+  INCQ    SI
+  ADDQ    $8, BX
+  JMP     start
 
 notfound:
-  MOVQ $0, return+32(FP)
-  RET ,
+  MOVQ    $0, return+32(FP)
+  RET
