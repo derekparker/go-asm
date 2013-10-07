05b14864849d299c5f8e714ce9bdb7bfb0b0af9d
Derek Parker <parkerderek86@gmail.com>
2013-10-07 12:09:55 -0500

    Move length is 8 bytes

--- vector/vector_amd64.s
+++ vector/vector_amd64.s
@@ -1,9 +1,8 @@
 TEXT ·Find+0(SB),$0
   MOVQ    $0, SI        // zero the iterator
   MOVQ    vec+0(FP), BX // BX = &vec[0]
-  MOVL    vec+8(FP), CX // len(vec)
+  MOVQ    vec+8(FP), CX // len(vec)
   MOVQ    num+24(FP), DX
-  MOVLQZX CX, CX        // CX as int64
 
 start:
   CMPQ    SI, CX
