70ba1eb51ffd7a3a90b450fbd2e8b12432dc40b4
Derek Parker <parkerderek86@gmail.com>
2013-10-09 16:50:09 -0500

    Typo

--- vector/sum_amd64.s
+++ vector/sum_amd64.s
@@ -1,6 +1,6 @@
 TEXT ·SumVec+0(SB), $0
   MOVQ vec1+0(FP), BX // Move the first vector into BX
-  MOVQ vec2+24(FP), CX // Move the first vector into BX
+  MOVQ vec2+24(FP), CX // Move the second vector into BX
   MOVUPS (BX), X0
   MOVUPS (CX), X1
   ADDPS X0, X1
