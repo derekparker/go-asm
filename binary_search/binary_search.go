4c6a8ec6cf697fe441cdea2c32140b5fbee68972
Derek Parker <parkerderek86@gmail.com>
2013-10-24 17:38:39 -0500

    Implement binary search in assembly

--- binary_search/binary_search.go
+++ binary_search/binary_search.go
@@ -1,18 +1,3 @@
 package bsearch
 
-func BinarySearch(arr []int, num int) bool {
-	length := len(arr)
-	half := length / 2
-
-	potential := arr[half]
-
-	if potential == num {
-		return true
-	} else if potential < num && length > 1 {
-		return BinarySearch(arr[half:], num)
-	} else if potential > num && length > 1 {
-		return BinarySearch(arr[:half], num)
-	}
-
-	return false
-}
+func BinarySearch(arr []int, num int) bool
